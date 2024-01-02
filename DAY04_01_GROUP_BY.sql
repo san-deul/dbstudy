51cfe83c66b666e68f6efa1647ada69490c70b8c
san-deul <bliss0208@naver.com>
2024-01-02 13:00:15 +0900

    04_01

--- DAY04_01_GROUP_BY.sql
+++ DAY04_01_GROUP_BY.sql
@@ -78,5 +78,41 @@ HAVING COUNT(*) >= 2; -- 통계함수를 이용한 조건은 HAVING절로 처리
 
 
 
+-- HR 계정으로 접속
 
 
+-- 1. 사원 테이블에서 동일한 부서번호를 가진 사원들을 그룹화하여 각 그룹별로 몇 명의 사원이 있는지 조회하시오.
+SELECT DEPARTMENT_ID 
+     , COUNT(*) 
+  FROM EMPLOYEES
+ GROUP BY DEPARTMENT_ID;
+
+
+-- 2. 사원 테이블에서 같은 직업을 가진 사원들을 그룹화하여 각 그룹별로 연봉의 평균이 얼마인지 조회하시오.
+SELECT JOB_ID
+     , FLOOR(AVG(SALARY))
+  FROM EMPLOYEES
+ GROUP BY JOB_ID;
+
+-- 3. 사원 테이블에서 전화번호 앞 3자리가 같은 사원들을 그룹화하여 각 그룹별로 연봉의 합계가 얼마인지 조회하시오.
+SELECT SUBSTR(PHONE_NUMBER,1,3)
+     , SUM(SALARY)
+  FROM EMPLOYEES
+ GROUP BY SUBSTR(PHONE_NUMBER,1,3);
+ --다시 !!!
+
+
+-- 4. 사원 테이블에서 각 부서별 사원수가 20명 이상인 부서를 조회하시오.
+SELECT DEPARTMENT_ID
+     , COUNT(*)
+  FROM EMPLOYEES
+ GROUP BY DEPARTMENT_ID
+HAVING COUNT(*) >=20;
+
+
+-- 5. 사원 테이블에서 각 부서별 사원수를 조회하시오. 단, 부서번호가 없는 사원은 제외하시오.
+SELECT DEPARTMENT_ID
+     , COUNT(*)
+  FROM EMPLOYEES
+ WHERE DEPARTMENT_ID IS NOT NULL
+ GROUP BY DEPARTMENT_ID;
